42d6bf2319a62a65380bd2ed234261153c61aa43
Chris Cosentino <chriscosentino@hotmail.com>
2020-05-31 20:41:20 -0400

    Updates

---
+++ precache-manifest.53f3fe9cae5c04eea7075d5572401acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36698d75d3941a4cad2ed7d9d2972b26",
+    "url": "/redux-restaurant-demo/index.html"
+  },
+  {
+    "revision": "64b7449c63a69bbf76d8",
+    "url": "/redux-restaurant-demo/static/css/main.43ecf0ba.chunk.css"
+  },
+  {
+    "revision": "3df981e87ac1020e7bd1",
+    "url": "/redux-restaurant-demo/static/js/2.ba174593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-restaurant-demo/static/js/2.ba174593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b7449c63a69bbf76d8",
+    "url": "/redux-restaurant-demo/static/js/main.7a51eb09.chunk.js"
+  },
+  {
+    "revision": "10e54ab13179e767586e",
+    "url": "/redux-restaurant-demo/static/js/runtime-main.c6b3835c.js"
+  }
+]);
